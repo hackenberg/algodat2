0238e4bed60b943a4de627fcdd8d9161d9ac5d33
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2012-05-21 18:47:56 +0200

    Angabe hinzugefügt

---
+++ ads1ss12/pa/KMST.java
@@ -0,0 +1,42 @@
+package ads1ss12.pa;
+
+import java.util.HashSet;
+
+/**
+ * Klasse zum Berechnen eines k-MST mittels Branch-and-Bound. Hier sollen Sie
+ * Ihre Lösung implementieren.
+ */
+public class KMST extends AbstractKMST {
+
+	/**
+	 * Der Konstruktor. Hier ist die richtige Stelle für die
+	 * Initialisierung Ihrer Datenstrukturen.
+	 * 
+	 * @param numNodes
+	 *            Die Anzahl der Knoten
+	 * @param numEdges
+	 *            Die Anzahl der Kanten
+	 * @param edges
+	 *            Die Menge der Kanten
+	 * @param k
+	 *            Die Anzahl der Knoten, die Ihr MST haben soll
+	 */
+	public KMST(Integer numNodes, Integer numEdges, HashSet<Edge> edges, int k) {
+		// TODO: Hier ist der richtige Platz fuer Initialisierungen
+	}
+
+	/**
+	 * Diese Methode bekommt vom Framework maximal 30 Sekunden Zeit zur
+	 * Verfügung gestellt um einen gültigen k-MST zu finden.
+	 * 
+	 * <p>
+	 * Fügen Sie hier Ihre Implementierung des Branch-and-Bound Algorithmus
+	 * ein.
+	 * </p>
+	 */
+	@Override
+	public void run() {
+		// TODO: Diese Methode ist von Ihnen zu implementieren
+	}
+
+}
